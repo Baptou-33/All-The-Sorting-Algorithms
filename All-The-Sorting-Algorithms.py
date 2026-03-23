a291a6e35ebdf8ec78da319395220189e9a79691
Baptou-33 <baptiste.dureau@outlook.fr>
2026-03-23 17:01:48 +0100

    Add a function to time the algorithm. I used eval, but this is the best way I've found to add easily a lot of differents sorting algorithms.

--- All-The-Sorting-Algorithms.py
+++ All-The-Sorting-Algorithms.py
@@ -1,4 +1,5 @@
 from random import *
+from time import *
 
 #Usefull functions------------------------------------------------------------------------------------------------------
 #Check if the list is well-sorted
@@ -19,7 +20,7 @@ def rng(l):
 
 
 #Sorting algorithms-----------------------------------------------------------------------------------------------------
-#Selection sort
+#Selection sort based algorithms
 def selectionSort(t, n):
     for i in range(n-1):
         inf = i
@@ -28,11 +29,43 @@ def selectionSort(t, n):
                 inf = j
         t[i], t[inf] = t[inf], t[i]
 
+def doubleSelectionSort(t, n):
+    for i in range(n//2):
+        inf = i
+        sup = i
+        for j in range(i+1, n-i):
+            if t[j] < t[inf]:
+                inf = j
+            elif t[j] > t[sup]:
+                sup = j
+        t[inf], t[i] = t[i], t[inf]
+        if sup == i:
+            sup = inf
+        t[sup], t[n-1-i] = t[n-1-i], t[sup]
+
 
 
 #Init code--------------------------------------------------------------------------------------------------------------
+#Sorting algorithm used
+algos = ["selectionSort", "doubleSelectionSort"]
+
 #One list for all the algorithms to be equal
 First_list = []
+
+#Time the sorting algorithm, test if the algorithm work, else, display the output
+def run(i):
+    #Init the list to sort
+    list = First_list.copy()
+
+    #Run and time the algo
+    start_time = perf_counter()
+    eval(algos[i] + "(list, number_of_data)")
+    end_time = perf_counter()
+    execution_time = end_time - start_time
+    print(algos[i], execution_time)
+
+    if not check_list(list):
+        print("\n", algos[i], "\nPour la liste initiale\t", First_list, "\nL'algorithme renvoie\t", list, "\n")
 
 
 
@@ -40,7 +73,7 @@ First_list = []
 if __name__ == "__main__":
     # Inputs
     default_value = 10000
-    number_of_data = input(f"Please enter the lenght of the random list to be sorted ('d' to put {default_value} from 0 to {default_value}) :\n")
+    number_of_data = input(f"Please enter the length of the random list to be sorted ('d' to put {default_value} from 0 to {default_value}) :\n")
     if number_of_data == "d":
         number_of_data = default_value
         min_random = 0
@@ -50,9 +83,10 @@ if __name__ == "__main__":
         min_random = int(input("Please enter the number min : "))
         max_random = int(input("Please enter the number max : "))
 
-#Create a random list
-rng(First_list)
+    #Create a random list
+    rng(First_list)
 
-selectionSort(First_list, number_of_data)
-print(First_list)
-print(check_list(First_list))
+    # Test all the algorithms
+    for i in range(len(algos)):
+        print(i + 1, "/", len(algos), " : ", algos[i])
+        run(i)
